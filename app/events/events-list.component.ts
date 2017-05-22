bad3b92d159eb778ef0040fa33f087fbabee5c38
kerem ozdemir <keremozdemir@kerem-MacBook-Pro.local>
2017-05-22 03:18:56 +0300

    using local variables to interact with child component

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -6,7 +6,9 @@ import { Component } from "@angular/core";
     <div>
         <h1>Upcoming Angular 2 Events</h1>
         <hr/>
-        <event-thumbnail (eventClick)="handleEventClicked($event)" [event]="event1"></event-thumbnail>
+        <event-thumbnail #thumbnail [event]="event1"></event-thumbnail>
+        <h3>{{thumbnail.someProperty}}</h3>
+        <button class="btn btn-primary" (click)="thumbnail.logFoo()">Log me some foo</button>
     </div>
     `
 })
@@ -24,8 +26,4 @@ export class EventsListComponent {
             country: 'England'
         }
     };
-
-    handleEventClicked (data) {
-        console.log('received: ', data);
-    }
 }

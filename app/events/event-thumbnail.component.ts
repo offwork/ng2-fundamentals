52483432d9e46e17f2b1d9a1098dbf5c08109781
kerem ozdemir <keremozdemir@kerem-MacBook-Pro.local>
2017-05-23 14:50:23 +0300

    hiding element with [hidden]

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -8,11 +8,11 @@ import { Component, Input } from "@angular/core";
         <div>Date: {{event?.date}}</div>
         <div>Time: {{event?.time}}</div>
         <div>Price: \${{event?.price}}</div>
-        <div *ngIf="event?.location">
+        <div [hidden]="!event?.location">
             <span>Location: {{event?.location?.address}}</span>
             <span class="ped-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
         </div>
-        <div *ngIf="event?.onlineUrl">Online Url: {{event?.onlineUrl}}</div>
+        <div [hidden]="!event?.onlineUrl">Online Url: {{event?.onlineUrl}}</div>
     </div>
     `,
     styles: [`

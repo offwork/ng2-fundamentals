af6485f25531b58914d1339c93f7805e7764187c
kerem ozdemir <keremozdemir@kerem-MacBook-Pro.local>
2017-06-20 20:00:49 +0300

    passing data input-output of a child component

--- app/events/event-details/create-session.component.ts
+++ app/events/event-details/create-session.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { ISessions, restrictedWords } from "../shared/index";
 
 @Component({
+    selector: 'create-session',
     templateUrl: 'app/events/event-details/create-session.component.html',
     styles: [`
         em {float:right; color:#E05C65; padding-left: 10px;}
@@ -14,6 +15,8 @@ import { ISessions, restrictedWords } from "../shared/index";
   `]
 })
 export class CreateSessionComponent implements OnInit {
+    @Output() saveNewSession = new EventEmitter();
+    @Output() cancelAddSession = new EventEmitter();
     newSessionForm: FormGroup;
     name: FormControl;
     presenter: FormControl;
@@ -48,6 +51,10 @@ export class CreateSessionComponent implements OnInit {
             voters: []
         };
 
-        console.log(session);
+        this.saveNewSession.emit(session);
+    }
+
+    cancel() {
+        this.cancelAddSession.emit();
     }
 }

db4fef14f66e9855c426bb95a28be6948330b6a2
kerem ozdemir <keremozdemir@kerem-MacBook-Pro.local>
2017-06-21 16:14:04 +0300

    sorting data

--- app/events/event-details/session-list.component.ts
+++ app/events/event-details/session-list.component.ts
@@ -8,11 +8,15 @@ import { ISessions } from "../shared/index";
 export class SessionListComponent implements OnChanges {
     @Input() sessions: ISessions[];
     @Input() filterBy: string;
+    @Input() sortBy: string;
     visibleSessions: ISessions[] = [];
 
     ngOnChanges() {
         if (this.sessions) {
             this.filterSessions(this.filterBy);
+            this.sortBy === 'name' ? 
+                this.visibleSessions.sort(sortByNameAsc) : 
+                this.visibleSessions.sort(sortByVotesDesc);
         }
     }
 
@@ -25,4 +29,18 @@ export class SessionListComponent implements OnChanges {
             });
         }
     }
+}
+
+function sortByNameAsc(s1: ISessions, s2: ISessions) {
+    if( s1.name > s2.name ) {
+        return 1;
+    } else if (s1.name === s2.name) {
+        return 0;
+    } else {
+        return -1;
+    }
+}
+
+function sortByVotesDesc(s1: ISessions, s2: ISessions) {
+    return s2.voters.length - s1.voters.length;
 }
